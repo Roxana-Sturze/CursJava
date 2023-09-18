d3d9631c2d2508edac7516780d558a3fc6ccec31
Roxana Sturze <roxanagabrielasturze@gmail.com>
2023-09-18 20:33:06 +0300

    curs5 commit2

--- src/curs5/DataTypes.java
+++ src/curs5/DataTypes.java
@@ -3,8 +3,17 @@ package curs5;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		byte nrMeuByte = 127;
+		short nrMeuShort = 32000;
+		int nrMeuInteger = 2000000000;
+		System.out.println(nrMeuShort);
+		
+		String text = "mar";
+		
+		char m = 'm';
 
 	}
+	
 
 }
