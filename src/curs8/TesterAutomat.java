64f568dbe0a740336d21f05c8de2deba5b3b8093
Roxana Sturze <roxanagabrielasturze@gmail.com>
2023-09-28 21:30:39 +0300

    curs8 final

---
+++ src/curs8/TesterAutomat.java
@@ -0,0 +1,18 @@
+package curs8;
+
+public class TesterAutomat extends Tester {
+	
+	private String programmingLanguage;
+
+	public String getProgrammingLanguage() {
+		return programmingLanguage;
+	}
+
+	public void setProgrammingLanguage(String programmingLanguage) {
+		this.programmingLanguage = programmingLanguage;
+	}
+	
+	
+	
+
+}
