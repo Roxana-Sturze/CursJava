3ecc93a405d2e3ba018b161ae3697b16cffd2879
Roxana Sturze <roxanagabrielasturze@gmail.com>
2023-10-02 20:38:34 +0300

    curs9 intermediar

---
+++ src/curs9/Cerc.java
@@ -0,0 +1,12 @@
+package curs9;
+
+public class Cerc extends Shape{
+	
+	
+	@Override
+	public void draw() {
+		System.out.println("Clasa CERC");
+		System.out.println("Deseneaza un cerc!");
+	}
+
+}
