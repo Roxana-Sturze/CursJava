31b0f75434243654f3b82afb46bb0aa3bbf5e177
Roxana Sturze <roxanagabrielasturze@gmail.com>
2023-10-05 21:27:06 +0300

    curs10

---
+++ src/curs10/InvalidAgeException.java
@@ -0,0 +1,11 @@
+package curs10;
+
+public class InvalidAgeException extends Exception{
+	
+	
+	public InvalidAgeException(String mesaj) {
+		super(mesaj);
+		
+	}
+
+}
---
+++ src/curs10/TestAgeVerifier.java
@@ -0,0 +1,25 @@
+package curs10;
+
+import java.util.Scanner;
+
+public class TestAgeVerifier {
+
+	public static void main(String[] args) {
+
+
+		AgeVerifier obj = new AgeVerifier();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter your age: ");
+		int age = scan.nextInt();
+		
+		try {
+			obj.checkAgeLimit(age);
+		} catch (InvalidAgeException e) {
+			
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
